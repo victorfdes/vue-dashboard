27ba4a369ade45154793823716ffc3e3063ebfeb
Victor Fernandes <victorfdes@users.noreply.github.com>
2020-02-24 18:36:53 +0530

    update footer and nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <v-app>
+    <v-navigation-drawer app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Vue Dashboard
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       app
       color="primary"
       dark
     >
     </v-app-bar>
+
     <v-content>
       <router-view></router-view>
+      <v-footer absolute>
+        Vue Dashboard &middot; &copy; 2020 &middot; Created by Victor Fernandes
+        <v-btn icon href="https://victor-fernandes.com" target="_blank">
+          <v-icon small>mdi-open-in-new</v-icon>
+        </v-btn>
+      </v-footer>
     </v-content>
-    <v-footer>
-      This is a footer
-    </v-footer>
   </v-app>
 </template>
 
